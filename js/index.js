299ff7d341846959e46687bb422743a20b02ad2a
Chinmay487 <pchinmay04@gmail.com>
2022-01-20 13:16:28 +0530

    navbar color changing on scroll

--- js/index.js
+++ js/index.js
@@ -3,8 +3,25 @@ const onBurgerClick = () => {
     const burger = document.getElementById("burger");
     if(menu.style.display === "none"){
         menu.style.display = "block";
+        burger.innerHTML = "X"
     } else {
         menu.style.display = "none";
+        burger.innerHTML = "&#9776;"
     }
 
 }
+
+var myNav = document.getElementById('navbar');
+window.onscroll =  () => { 
+    "use strict";
+    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50 ) {
+        myNav.classList.add("bg_colored");
+        myNav.classList.remove("bg_transparent");
+    } 
+    else {
+        myNav.classList.add("bg_transparent");
+        myNav.classList.remove("bg_colored");
+    }
+};
+
+
